05b9bc9ee3e1e24b34fc10080d2618075a69a4e5
Carlos Fernandes <carlosfernandes@Carloss-MBP.local>
2025-03-11 09:04:29 -0300

    corretion docker

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -6,7 +6,7 @@ def init_db(app):
     """
     Inicializa a base de dados com o app Flask e o SQLAlchemy.
     """
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@mysql:3306/market_management'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@mysql57:3306/market_management'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
