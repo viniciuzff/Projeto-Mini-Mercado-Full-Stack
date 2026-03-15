624c2b969bf05ca469bd3ca22cf1c9f5541cc5de
Vinicius <viniciusviff14@gmail.com>
2026-03-15 19:26:53 -0300

    feat: atualização do model user para cadastro de seller

--- src/Infrastructure/Model/user.py
+++ src/Infrastructure/Model/user.py
@@ -1,15 +1,22 @@
-from src.config.data_base import db 
+from src.config.data_base import db
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    cnpj = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    status = db.Column(db.Boolean, default=False)
+    verification_code = db.Column(db.String(4))
 
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
+            "cnpj": self.cnpj,
             "email": self.email,
-            "password": self.password
+            "phone": self.phone,
+            "status": self.status
         }
