651cfd5042ef1419e6357299ee7b5f67aa11a387
Vinicius <viniciusviff14@gmail.com>
2026-03-15 23:24:46 -0300

    feat: cadastro de seller com envio de código via WhatsApp (Twilio)

--- src/Application/Controllers/user_controller.py
+++ src/Application/Controllers/user_controller.py
@@ -2,18 +2,23 @@ from flask import request, jsonify, make_response
 from src.Application.Service.user_service import UserService
 
 class UserController:
+
     @staticmethod
     def register_user():
         data = request.get_json()
+
         name = data.get('name')
+        cnpj = data.get('cnpj')
         email = data.get('email')
+        phone = data.get('phone')
         password = data.get('password')
 
-        if not name or not email or not password:
+        if not name or not cnpj or not email or not phone or not password:
             return make_response(jsonify({"erro": "Missing required fields"}), 400)
 
-        user = UserService.create_user(name, email, password)
+        user = UserService.create_user(name, cnpj, email, phone, password)
+
         return make_response(jsonify({
-            "mensagem": "User salvo com sucesso",
-            "usuarios": user.to_dict()
+            "mensagem": "Seller cadastrado com sucesso",
+            "usuario": user.to_dict()
         }), 200)
--- src/Application/Service/user_service.py
+++ src/Application/Service/user_service.py
@@ -1,11 +1,30 @@
+import random
 from src.Domain.user import UserDomain
 from src.Infrastructure.Model.user import User
-from src.config.data_base import db 
+from src.config.data_base import db
+from src.Infrastructure.http.whats_app import WhatsApp
+
 
 class UserService:
+
     @staticmethod
-    def create_user(name, email, password):        
-        user = User(name=name, email=email, password=password)        
+    def create_user(name, cnpj, email, phone, password):
+
+        code = str(random.randint(1000,9999))
+
+        user = User(
+            name=name,
+            cnpj=cnpj,
+            email=email,
+            phone=phone,
+            password=password,
+            status=False,
+            verification_code=code
+        )
+
         db.session.add(user)
-        db.session.commit()       
+        db.session.commit()
+
+        WhatsApp.whats_app(code)
+
         return UserDomain(user.id, user.name, user.email, user.password)
