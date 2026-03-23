ad23b60b40a3c1e11038f3b719f3e3212c842aaf
Vinicius <viniciusviff14@gmail.com>
2026-03-22 22:13:09 -0300

    feat: subindo arquivo whats

---
+++ src/Infrastructure/http/whats_app.py
@@ -0,0 +1,17 @@
+from twilio.rest import Client
+
+class WhatsApp:
+
+    @staticmethod
+    def whats_app(code):
+
+        account_sid = "SEU_ACCOUNT_SID"
+        auth_token = "SEU_AUTH_TOKEN"
+
+        client = Client(account_sid, auth_token)
+
+        client.messages.create(
+            body=f"Seu código de ativação é: {code}",
+            from_='whatsapp:+14155238886',
+            to='whatsapp:+5511988410000'
+        )
